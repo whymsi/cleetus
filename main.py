dc04546b7ecfa6264e0cc366d547401581f9d530
void <landonsvatek71@gmail.com>
2025-01-29 10:09:31 -0800

    avatar and ping

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 import os
+from discord import Embed
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -15,6 +16,23 @@ async def on_ready():
 @bot.command()
 async def hello(ctx):
     await ctx.send('Hello, World!')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'Pong! Latency: {round(bot.latency * 1000)}ms')
+
+
+@bot.command(name="av")
+async def avatar(ctx, member: discord.Member = None):
+    if not member:
+        member = ctx.author
+
+    embed = Embed(
+        title = f"{member.name}'s avatar",
+        url = member.avatar,
+        color = member.color
+        ).set_image(url = member.avatar.url)
+    await ctx.send(embed=embed)
 
 TOKEN = os.getenv('TOKEN')
 
